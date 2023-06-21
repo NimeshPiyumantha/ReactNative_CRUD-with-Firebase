e4abe2c6de4c12a6509f82ff06417ad413e4bafb
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-21 06:48:50 +0530

    Update Content

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>React Native CRUD Firebase</Text>
       <StatusBar style="auto" />
     </View>
   );

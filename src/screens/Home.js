d0375275a5df054365898788fa03bfc91cbf91c0
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 08:26:52 +0530

    Update Content

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { StyleSheet, View, SafeAreaView, Text, Alert } from "react-native";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  Image,
+} from "react-native";
 import Button from "../components/Button";
+import { StatusBar } from "expo-status-bar";
 
 const Home = ({ navigation }) => {
   const onPressUser = () => {
@@ -8,22 +16,39 @@ const Home = ({ navigation }) => {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.fixToButton}>
-        <Button onPress={onPressUser} color="#841584" title="User Manage" />
-        <Button
-          title="Item Manage"
-          color="#841524"
-          onPress={() => Alert.alert("Right button pressed")}
-        />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container1}>
+      <StatusBar style="light" />
+      <Image
+        blurRadius={70}
+        source={require("../assets/images/bg.png")}
+        style={styles.imageContainer}
+      />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.fixToButton}>
+          <Button onPress={onPressUser} color="#841584" title="User Manage" />
+          <Button
+            title="Item Manage"
+            color="#841524"
+            onPress={() => Alert.alert("Right button pressed")}
+          />
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     flex: 1,
+    position: "relative",
+  },
+  imageContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  container: {
+    marginVertical: 30,
     justifyContent: "center",
     marginHorizontal: 16,
   },

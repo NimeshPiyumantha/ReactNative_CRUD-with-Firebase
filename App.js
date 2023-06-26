5b15d8a95b4f99cef175ba72c7ef5ccd0031b78a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 07:03:21 +0530

    Update Content

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-
-import { View, Image } from "react-native";
-
-
+import AppNavigation from "./src/navigation/AppNavigation";
 
 export default function App() {
-  return (
-      <View className="flex-1 relative">
-        <StatusBar style="light"></StatusBar>
-        <Image
-          blurRadius={70}
-          source={require("./src/assets/images/bg.png")}
-          className="absolute w-full h-full"
-        />
-      </View>
-  );
+  return <AppNavigation />;
 }

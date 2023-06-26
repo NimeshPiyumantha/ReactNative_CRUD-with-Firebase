cbe888e4864673856a5ada4e07583ae2f384d9ad
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 06:54:28 +0530

    Update Content

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import Home from "./screens/Home";
-import UserForm from "./screens/UserForm";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import { View, Image } from "react-native";
 
-const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <>
+    
       <View className="flex-1 relative">
         <StatusBar style="light"></StatusBar>
         <Image
@@ -18,28 +15,5 @@ export default function App() {
           className="absolute w-full h-full"
         />
       </View>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Dashboard"
-          screenOptions={{
-            headerStyle: { backgroundColor: "#351f44" },
-            headerTintColor: "#fff",
-            headerTitleStyle: { fontWeight: "bold" },
-            contentStyle: { backgroundColor: "rgba(255,255,255,0.2)" },
-          }}
-        >
-          <Stack.Screen
-            name="HomeForm"
-            component={Home}
-            options={{ title: "Home Screen" }}
-          />
-          <Stack.Screen
-            name="UserForm"
-            component={UserForm}
-            options={{ title: "User Screen" }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
   );
 }
---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Home from "../screens/Home";
+import UserForm from "../screens/UserForm";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { LogBox } from "react-native";
+
+const Stack = createNativeStackNavigator();
+
+LogBox.ignoreLogs([
+  "Non-serializable values were found in the navigation state",
+]);
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Dashboard"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#351f44" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+          contentStyle: { backgroundColor: "rgba(255,255,255,0.2)" },
+        }}
+      >
+        <Stack.Screen
+          name="HomeForm"
+          component={Home}
+          options={{ title: "Home Screen" }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{ title: "User Screen" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}

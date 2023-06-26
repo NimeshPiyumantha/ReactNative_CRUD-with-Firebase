4969ed07721d655131cbcc8944a259ed5ef51f93
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 06:46:31 +0530

    Update Content

--- App.js
+++ App.js
@@ -3,13 +3,21 @@ import Home from "./screens/Home";
 import UserForm from "./screens/UserForm";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Image } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light"></StatusBar>
+      <View className="flex-1 relative">
+        <StatusBar style="light"></StatusBar>
+        <Image
+          blurRadius={70}
+          source={require("./assets/bg.png")}
+          className="absolute w-full h-full"
+        />
+      </View>
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="Dashboard"
@@ -17,7 +25,7 @@ export default function App() {
             headerStyle: { backgroundColor: "#351f44" },
             headerTintColor: "#fff",
             headerTitleStyle: { fontWeight: "bold" },
-            contentStyle: { backgroundColor: "#ecf0f1" },
+            contentStyle: { backgroundColor: "rgba(255,255,255,0.2)" },
           }}
         >
           <Stack.Screen

2bfc06cb422f0ffc5237f3f8f43c104dc4c0a544
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-29 07:15:28 +0530

    update Content

---
+++ BackEnd/src/controllers/UserController.ts
@@ -0,0 +1,127 @@
+import { RequestHandler, Request, Response } from "express";
+import mongoose, { ClientSession } from "mongoose";
+import { User } from "../modules/User";
+
+export default class UserController {
+  addNewUser: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    let session: ClientSession | null = null;
+
+    try {
+      const user = new User(req.body);
+      // save role details
+      let newUser = await user.save();
+      return res
+        .status(200)
+        .json({ message: "New User created.", responseData: newUser });
+    } catch (error: unknown) {
+      if (session != null) {
+      }
+
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
+
+  updateUser: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      let updatedUser = await User.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      return res
+        .status(200)
+        .json({ message: "User updated.", responseData: updatedUser });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
+
+  deleteUser: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      let deletedUser = await User.findByIdAndDelete(id);
+
+      if (!deletedUser) {
+        throw new Error("Failed to delete User.");
+      }
+
+      return res
+        .status(200)
+        .json({ message: "User deleted.", responseData: deletedUser });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
+
+  retrieveAllUser: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      const users = await User.find();
+      return res.status(200).json({ responseData: users });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
+
+  searchUserFirstName: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      const users = await User.find({
+        $or: [{ fristName: id }, { lastName: id }, { address: id }],
+      });
+      return res.status(200).json({ responseData: users });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
+
+  searchById: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      const users = await User.findById(id);
+      return res.status(200).json({ responseData: users });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
+}
